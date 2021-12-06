232780def9bce17cf239d22dc1e14ea00e4abf98
Tubbles <jae91m@gmail.com>
2021-12-06 16:11:20 +0100

    Even smaller MVE

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use ggez::conf;
-use ggez::event::{self, Button, EventHandler, GamepadId, KeyCode, KeyMods};
+use ggez::event::{self, EventHandler};
 use ggez::Context;
 use ggez::GameResult;
 use std::time::Instant;
@@ -36,32 +36,12 @@ impl EventHandler for MainState {
     fn draw(&mut self, _ctx: &mut Context) -> GameResult<()> {
         Ok(())
     }
-
-    fn key_down_event(
-        &mut self,
-        ctx: &mut Context,
-        keycode: KeyCode,
-        keymods: KeyMods,
-        _repeat: bool,
-    ) {
-        if keycode == KeyCode::Q && keymods.contains(KeyMods::CTRL) {
-            ggez::event::quit(ctx);
-        }
-    }
-    fn gamepad_button_down_event(&mut self, ctx: &mut Context, button: Button, _id: GamepadId) {
-        if button == Button::Mode {
-            ggez::event::quit(ctx);
-        }
-    }
 }
 
 pub fn main() -> GameResult {
     let start = Instant::now();
-    let cb = ggez::ContextBuilder::new("super_simple", "ggez").window_setup(
-        conf::WindowSetup::default()
-            .title("super_simple")
-            .vsync(true),
-    );
+    let cb = ggez::ContextBuilder::new("Gamepad Start Lag MVE", "Tubbles")
+        .window_setup(conf::WindowSetup::default());
     let (ctx, event_loop) = cb.build()?;
     let state = MainState::new(start)?;
     event::run(ctx, event_loop, state)
