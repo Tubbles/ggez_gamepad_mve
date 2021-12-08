c60275982d458e6975943c5ba9b79853fee177f1
Tubbles <jae91m@gmail.com>
2021-12-08 16:29:51 +0100

    Add two more time instants

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ use std::time::Instant;
 
 struct MainState {
     start: Instant,
+    builder: Instant,
+    context: Instant,
     first_loop: bool,
 }
 
 impl MainState {
-    pub fn new(start: Instant) -> GameResult<MainState> {
+    pub fn new(start: Instant, builder: Instant, context: Instant) -> GameResult<MainState> {
         let this = MainState {
             start,
+            builder,
+            context,
             first_loop: true,
         };
         Ok(this)
@@ -24,7 +28,15 @@ impl EventHandler for MainState {
         let now = Instant::now();
         if self.first_loop {
             println!(
-                "Time to first loop: {} s",
+                "Time at create builder: {} s",
+                self.builder.duration_since(self.start).as_secs_f32()
+            );
+            println!(
+                "Time at build context: {} s",
+                self.context.duration_since(self.start).as_secs_f32()
+            );
+            println!(
+                "Time at first loop: {} s",
                 now.duration_since(self.start).as_secs_f32()
             );
             self.first_loop = false;
@@ -42,7 +54,9 @@ pub fn main() -> GameResult {
     let start = Instant::now();
     let cb = ggez::ContextBuilder::new("Gamepad Start Lag MVE", "Tubbles")
         .window_setup(conf::WindowSetup::default());
+    let builder = Instant::now();
     let (ctx, event_loop) = cb.build()?;
-    let state = MainState::new(start)?;
+    let context = Instant::now();
+    let state = MainState::new(start, builder, context)?;
     event::run(ctx, event_loop, state)
 }
